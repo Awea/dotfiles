796ecc7c611f309ec446f08da965ac43a563ecb4
Awea <aweaoftheworld@gmail.com>
2020-09-25 10:56:51 +0200

    🔧 Add usual git aliases

---
+++ git/aliases.zsh
@@ -0,0 +1,18 @@
+# Git
+alias g='git'
+
+# Commit (c)
+alias gcm='git commit --message'
+
+# Fetch (f)
+alias gfa='git fetch --all'
+alias gfr='git pull --rebase'
+
+# Push (p)
+alias gp='git push'
+alias gpF='git push --force'
+alias gpA='git push --all && git push --tags'
+
+# Stash (s)
+alias gs='git stash'
+alias gsp='git stash pop'
