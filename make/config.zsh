909614049739ec1123197d755e06d1006341b419
Awea <aweaoftheworld@gmail.com>
2021-06-02 02:34:34 +0200

    🔧 Add Make default flags

---
+++ make/config.zsh
@@ -0,0 +1,6 @@
+#! /usr/bin/env zsh
+
+# MAKEFLAGS to define make default flags:
+# - `-j 4` number of simultaned jobs
+# - `--no-print-directory` obviously
+export MAKEFLAGS='-j 4 --no-print-directory' # -s'
