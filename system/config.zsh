49787229a0ec7cafd7d8d9a1a43788d1115ab5a8
Awea <aweaoftheworld@gmail.com>
2022-08-17 10:55:04 +0200

    ✨ system: enable history for iex session

--- system/config.zsh
+++ system/config.zsh
@@ -2,3 +2,6 @@
 # -n = in a new window
 # -w = tell command line to wait until document is saved and closed
 export EDITOR="/usr/bin/subl -n -w"
+
+# Enable history for iex session
+export ERL_AFLAGS="-kernel shell_history enabled"
